5afe5f583d911e9bded2b3aed3a9841850a1a540
MALARKODI-R <9750427573.ar@gmail.com>
2024-09-13 23:04:50 +0530

    Add files via upload

---
+++ Add 2 Nos.c
@@ -0,0 +1,9 @@
+//program to add two integers
+#include<stdio.h>
+void main(){
+  int a,b,c;
+  printf("Enter two numbers: ");//the numbers should be in integer format
+  scanf("%d %d",&a,&b);
+  c=a+b;
+  printf("The sum of given integer is %d",c);
+  }
---
+++ Own Name.c
@@ -0,0 +1,7 @@
+//program to print your own name
+#include<stdio.h>
+void main(){
+    char a[100];
+    printf("Enter your name: ");
+    scanf("%s",&a);
+    printf("Your name is %s",a);}
