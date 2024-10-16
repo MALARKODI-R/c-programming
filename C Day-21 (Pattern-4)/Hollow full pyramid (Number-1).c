0cdb621ee732d7cd785c2ff873bf3e1e7d9d4214
MALARKODI-R <9750427573.ar@gmail.com>
2024-10-16 23:06:51 +0530

    Add files via upload

---
+++ C Day-21 (Pattern-4)/Hollow full pyramid (Number-1).c
@@ -0,0 +1,22 @@
+//Print hollow full pyramid
+#include <stdio.h>
+int main()
+{
+    int a,i,j,k;
+    printf("Enter number of rows: ");
+    scanf("%d",&a);
+    for (i=0;i<a;i++) {
+        for (j=0;j<2*(a-i)-1;j++) {
+            printf(" ");
+        }
+        for (k=0;k<2*i+1;k++) {
+            if (k==0 || k==2*i || i==a-1) {
+                printf("%d ",i);
+            }
+            else {
+                printf("  ");
+            }
+        }
+        printf("\n");
+    }
+}
