749272d21fae2c716acff065ba760c7b8fd45e95
MALARKODI-R <9750427573.ar@gmail.com>
2024-10-17 23:22:40 +0530

    Add files via upload

---
+++ C Day-22 (Pattern-5)/Diamond pattern( Number-2 ).c
@@ -0,0 +1,19 @@
+// To print diamond pattern
+#include <stdio.h>
+int main()
+{
+    int i,j,k,a,c;
+    printf("Enter a number: ");
+    scanf("%d",&a);
+    for(i=0;i<2*a-1;i++){
+        if(i<a){
+            c=2*(a-i)-1;}
+        else{
+            c=2*(i-a+1)+1;}
+        for(j=0;j<c;j++){
+            printf(" ");}
+        for(k=0;k<2*a-c;k++){
+            printf("%d ",j);}
+        printf("\n");
+    }
+}
